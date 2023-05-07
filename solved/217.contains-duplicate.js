9ebf5700ff4fa75e6eaf1bdf810bfc5816d250f6
alibacova <libacova.a@gmail.com>
2023-05-06 19:37:56 -0700

    217 solved

--- solved/217.contains-duplicate.js
+++ solved/217.contains-duplicate.js
@@ -10,7 +10,17 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
+  let allNums = new Set(nums);
+  return allNums.size !== nums.length;
+  // let allNums = {};
+  // for (let num of nums) {
+  //     if (!allNums[num]) {
+  //         allNums[num] = 1;
+  //     } else {
+  //         return true;
+  //     }
+  // }
+  // return false;
 };
 // @lc code=end
 
